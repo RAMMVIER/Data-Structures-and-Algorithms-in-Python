a1132960afdc19005a3503ea597f5935bfa18a43
Rammvier <892232053@qq.com>
2020-02-20 15:43:34 -0500

    Update list.py

--- data strcture/list.py
+++ data strcture/list.py
@@ -62,12 +62,12 @@ class linked_list:
 # Test area
 my_list = linked_list()
 
-my_list.append(1)
-my_list.append(2)
-my_list.append(3)
-my_list.append(4)
+# my_list.append(1)
+# my_list.append(2)
+# my_list.append(3)
+# my_list.append(4)
 
-my_list.erase(2)
+# my_list.erase(2)
 
 my_list.display()
 
