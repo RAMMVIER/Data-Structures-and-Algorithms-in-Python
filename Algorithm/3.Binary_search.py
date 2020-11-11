73aa47c70225a1963d14572936e241560f047bce
Rammvier <892232053@qq.com>
2020-11-12 02:39:18 +0800

    Update 3.Binary_search.py

--- Algorithm/3.Binary_search.py
+++ Algorithm/3.Binary_search.py
@@ -1,3 +1,21 @@
 # 二分查找：从有序列表的初始候选区list[0:n-1]开始，通过对待查找的值与候选区中间值的比较，使候选区减小一半
 
+
 def binary_search(li, val):
+    left = 0
+    right = len(li) - 1
+    while left <= right:                # 候选区有值
+        mid = (left + right) // 2       # 整除用于取整数部分作为下标
+        if li[mid] == val:
+            return mid
+        elif li[mid] > val:             # 待查找的值在mid左侧
+            right = mid - 1
+        else:                           # 待查找的值在mid右侧
+            left = mid + 1
+    else:
+        return None
+
+
+# test
+li = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(binary_search(li, 7))
