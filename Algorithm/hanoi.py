87c4134f14840b0a3d650f0aeb3baf5d0c8373fd
Rammvier <892232053@qq.com>
2020-11-12 01:09:40 +0800

    update

---
+++ Algorithm/hanoi.py
@@ -0,0 +1,19 @@
+# 汉诺塔递归，n为盘子数量，abc参数为柱子
+# abc其顺序含义为，从a经过b移动至c
+
+# 汉诺塔问题求解：
+# n个盘子时：
+#   1.把n-1个圆盘从a经过c移动到b
+#   2.把第n个圆盘从a移动到c
+#   3.把n-1个圆盘从b经过a移动到c
+
+
+
+def hanoi(n, a, b, c):
+    if n > 0:
+        hanoi(n - 1, a, c, b)
+        print("Moving from %s to %s" % (a, c))
+        hanoi(n - 1, b, a, c)
+
+# test
+hanoi(3, 'A', 'B', 'C')
