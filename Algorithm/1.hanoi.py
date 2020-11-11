572d465352b485af8499db9871a1e2a007ab45d7
Rammvier <892232053@qq.com>
2020-11-12 01:16:23 +0800

    Update 1.hanoi.py

--- Algorithm/1.hanoi.py
+++ Algorithm/1.hanoi.py
@@ -8,12 +8,12 @@
 #   3.把n-1个圆盘从b经过a移动到c
 
 
-
 def hanoi(n, a, b, c):
     if n > 0:
         hanoi(n - 1, a, c, b)
         print("Moving from %s to %s" % (a, c))
         hanoi(n - 1, b, a, c)
+
 
 # test
 hanoi(7, 'A', 'B', 'C')
