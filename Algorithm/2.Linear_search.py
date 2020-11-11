67c5a5d8314b5f40f05393f0ebe214ba1d2ed0f1
Rammvier <892232053@qq.com>
2020-11-12 01:37:19 +0800

    Create 2.Linear_search.py

---
+++ Algorithm/2.Linear_search.py
@@ -0,0 +1,17 @@
+# 顺序查找：线性查找，从列表第一个元素开始顺序搜索，直到找到元素或搜索至列表最后一个元素为止
+# 查找元素存在，返回元素下标
+# 查找元素不存在，返回None或-1
+# 时间复杂度：O(n)
+
+
+def linear_search(data_list, val):
+    for index, v in enumerate(data_list):
+        if v == val:
+            return index
+    else:
+        return None
+
+
+# test
+data = [5, 10, 14, 18, 29, 45, 73]
+print(linear_search(data, 45))
