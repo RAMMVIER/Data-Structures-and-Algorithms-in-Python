5d0ddf672b55cc1e42f78ea01dab9f1110ee4a63
Rammvier <892232053@qq.com>
2020-11-13 03:34:18 +0800

    Update 4.Select_sort.py

--- Algorithm/4.Select_sort.py
+++ Algorithm/4.Select_sort.py
@@ -1,8 +1,9 @@
 # 选择排序，每次遍历中选择最小的元素，与列表第一个元素交换位置，实现原地排序
+# 时间复杂度：O(n^2)
 
 
 # 简单版选择排序，需要两个list，且需要对原list进行min、append、remove操作，效率过低
-def select_sort_simple(li):                # 时间复杂度：O(n^2)
+def select_sort_simple(li):
     li_new = []
     for i in range(len(li)):
         min_val = min(li)
