a8464025a7a6da1eabd53ec9febe9a49a9513d4c
Rammvier <892232053@qq.com>
2020-11-13 02:38:07 +0800

    Update 3.Bubble_sort.py

--- Algorithm/3.Bubble_sort.py
+++ Algorithm/3.Bubble_sort.py
@@ -10,9 +10,14 @@ import random
 
 def bubble_sort(li):
     for i in range(len(li) - 1):            # 第i次遍历
+        exchange = False
         for j in range(len(li) - i - 1):
             if li[j] > li[j + 1]:
                 li[j], li[j + 1] = li[j + 1], li[j]
+                exchange = True
+        print(li)
+        if not exchange:
+            return
 
 
 # test
