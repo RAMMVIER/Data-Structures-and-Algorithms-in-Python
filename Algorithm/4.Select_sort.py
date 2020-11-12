acf84eff072a62ba163d613b1b11c1d844a61c0b
Rammvier <892232053@qq.com>
2020-11-13 03:26:25 +0800

    Update 4.Select_sort.py

--- Algorithm/4.Select_sort.py
+++ Algorithm/4.Select_sort.py
@@ -12,12 +12,16 @@ def select_sort_simple(li):                # 时间复杂度：O(n^2)
 
 
 def select_sort(li):
-    for i in range(len(li) - 1):        # i为遍历次数，最后一个元素必是最大值，无需排列，因此为n-1次遍历
-        min_location = i                # 记录当前最小值的位置，便于进行对比，每次循环开始时，设置为当前循环的起点i
-        for j in range(i + 1, len(li)):
+    for i in range(len(li) - 1):            # i为遍历次数，最后一个元素必是最大值，无需排列，因此为n-1次遍历
+        min_location = i                    # 记录当前最小值的位置，便于进行对比，每次循环开始时，设置为当前循环的起点i
+        for j in range(i + 1, len(li)):     # 在无序区进行遍历，寻找无序区内的最小值
             if li[j] < li[min_location]:
                 min_location = j
+        li[i], li[min_location] = li[min_location], li[i]
+    return li
+
 
 # test
 test_list = [3, 4, 6, 7, 8, 1, 9, 2, 5]
-print(select_sort_simple(test_list))
+# print(select_sort_simple(test_list))
+print(select_sort(test_list))
