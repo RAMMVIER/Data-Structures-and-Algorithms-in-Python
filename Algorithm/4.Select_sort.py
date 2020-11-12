44e595a6ef6ec219df70f0243b1b332d711fdee7
Rammvier <892232053@qq.com>
2020-11-13 03:06:12 +0800

    update

---
+++ Algorithm/4.Select_sort.py
@@ -0,0 +1,15 @@
+# 选择排序，每次遍历中选择最小的元素
+
+
+def select_sort(li):
+    li_new = []
+    for i in range(li):
+        min_val = min(li)
+        li_new.append(min_val)
+        li.remove(min_val)
+    return li_new
+
+
+# test
+test_list = [3, 4, 6, 7, 8, 1, 9, 2, 5]
+print(select_sort(test_list))
