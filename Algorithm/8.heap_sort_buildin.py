7d8f228a1604dd6c8a229ef45d58cbeecb20205a
Rammvier <892232053@qq.com>
2020-11-19 05:35:06 +0800

    Update 8.heap_sort_buildin.py

--- Algorithm/8.heap_sort_buildin.py
+++ Algorithm/8.heap_sort_buildin.py
@@ -7,3 +7,12 @@ test_list = list(range(100))
 random.shuffle(test_list)
 
 print(test_list)
+
+# 建堆(default为小根堆)
+heapq.heapify(test_list)
+print(test_list)
+
+# 弹出一个最小元素
+n = len(test_list)
+for i in range(n):
+    print(heapq.heappop(test_list), end=',')
