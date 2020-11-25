a8d23c67258ae2e9d610f2aebe1ee3cce7fe1038
Rammvier <892232053@qq.com>
2020-11-26 01:10:24 +0800

    Update 12.Count_sort.py

--- Algorithm/12.Count_sort.py
+++ Algorithm/12.Count_sort.py
@@ -1,8 +1,21 @@
 # 计数排序：
 
 
+import random
+
+
 def count_sort(li, max_count=100):
     count = [0 for _ in range(max_count + 1)]
     for val in li:
         count[val] += 1
-        
+    li.clear()
+    for index, val in enumerate(count):
+        for i in range(val):
+            li.append(index)
+
+
+# test
+test_list = list(range(100))
+random.shuffle(test_list)
+count_sort(test_list)
+print(test_list)
