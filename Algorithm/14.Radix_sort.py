67b6baa9e76853af817c57d835781c65acdc7104
Rammvier <892232053@qq.com>
2020-11-30 01:37:39 +0800

    Update 14.Radix_sort.py

--- Algorithm/14.Radix_sort.py
+++ Algorithm/14.Radix_sort.py
@@ -1,2 +1,29 @@
-# 基数排序：
+# 基数排序：先根据个位数字大小进行排序，之后根据十位排序，以此类推
 
+
+import random
+
+
+def radix_sort(li):
+    # 确定列表中的最大值，最大值有几位数字则需要进行几次排序
+    max_num = max(li)
+    iteration = 0
+    while 10 ** iteration <= max_num:
+        buckets = [[] for _ in range(10)]
+        for var in li:
+            # 取出当前迭代对应的位上的数字
+            digit = (var // 10 ** iteration) % 10
+            buckets[digit].append(var)
+        # 分桶结束
+        li.clear()
+        for buc in buckets:
+            li.extend(buc)
+        # 把数字写回原列表
+        iteration += 1
+
+
+# test
+test_list = list(range(1000))
+random.shuffle(test_list)
+radix_sort(test_list)
+print(test_list)
