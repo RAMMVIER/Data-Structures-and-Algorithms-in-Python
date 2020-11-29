e8386d17259c6174680f5d7858020825690176be
Rammvier <892232053@qq.com>
2020-11-30 02:16:35 +0800

    Update Q1.py

--- Algorithm/Test Quiz/Q1.py
+++ Algorithm/Test Quiz/Q1.py
@@ -2,5 +2,18 @@
 #   s = "anagram", t = "nagaram", return True
 #   s = "rat", t = "car", return False
 
+# 解法：对两个字符串进行排序，将两者对比作为返回值
 
+def isAnagram(self, s, t):
+    """
+    :param self:
+    :param s: str
+    :param t: str
+    :return: bool
+    """
 
+    ss = list(s)
+    tt = list(t)
+    ss.sort()
+    tt.sort()
+    return ss == tt
