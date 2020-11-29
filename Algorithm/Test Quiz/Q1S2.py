dfea015c4b7205126bb42a07c1fec1739c1a8120
Rammvier <892232053@qq.com>
2020-11-30 02:32:43 +0800

    Update Q1S2.py

--- Algorithm/Test Quiz/Q1S2.py
+++ Algorithm/Test Quiz/Q1S2.py
@@ -1,4 +1,4 @@
-# 解法2：使用字典记录所有字母出现的次数
+# 解法2：使用两个字典分别记录所有字母在两个字符串中出现的次数，之后进行对比
 
 
 def isAnagram(self, s, t):
@@ -9,3 +9,10 @@ def isAnagram(self, s, t):
     :return: bool
     """
 
+    dict1 = {}
+    dict2 = {}
+    for ch in s:
+        dict1[ch] = dict1.get(ch, 0) + 1
+    for ch in t:
+        dict2[ch] = dict2.get(ch, 0) + 1
+    return dict1 == dict2
