84385e76ae17f2b415a8265aebd3c474491364f3
Rammvier <892232053@qq.com>
2020-11-30 02:39:56 +0800

    Update Q2.py

--- Algorithm/Test Quiz/Q2.py
+++ Algorithm/Test Quiz/Q2.py
@@ -1,2 +1,14 @@
+# 解法1：遍历矩阵进行查找
 
+def searchMatrix(self, matrix, target):
+    """
+    :param self:
+    :param matrix: List[List[int]]
+    :param target: int
+    :return: bool
+    """
 
+    for line in matrix:
+        if target in line:
+            return True
+    return False
