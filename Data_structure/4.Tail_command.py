b86b4f6c49b0c8efd3228da61853b73ef840dc62
Rammvier <892232053@qq.com>
2020-12-04 05:27:48 +0800

    Update 4.Tail_command.py

--- Data_structure/4.Tail_command.py
+++ Data_structure/4.Tail_command.py
@@ -1,1 +1,15 @@
+# 使用队列实现Linux中的tail操作
+
+
 from collections import deque
+
+
+def tail(n):
+    with open('test.txt', 'r') as f:
+        q = deque(f, n)
+        return q
+
+
+# print(tail(5))
+for line in tail(5):
+    print(line, end='')
