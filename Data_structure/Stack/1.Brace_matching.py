cb6bd537a692d17c579440c860670979e4c7f204
Rammvier <892232053@qq.com>
2020-12-04 03:22:44 +0800

    Create 1.Brace_matching.py

---
+++ Data_structure/Stack/1.Brace_matching.py
@@ -0,0 +1,34 @@
+# 括号匹配：给定一串括号字符，确定其中的括号是否能匹配
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, element):
+        self.stack.append(element)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def get_top(self):
+        if len(self.stack) > 0:
+            return self.stack[-1]
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+
+def brace_match(s):
+    match = {'}':'{', ']':'[', ')':'('}
+    stack = Stack()
+    for ch in s:
+        if ch in {'(', '[', '{'}:
+            stack.push(ch)
+        else:
+            if stack.is_empty():
+                return False
+            else:
+                
