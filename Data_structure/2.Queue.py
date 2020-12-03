0869e07d2c799b14218ddca4636e439a160f7562
Rammvier <892232053@qq.com>
2020-12-04 05:13:07 +0800

    Update 2.Queue.py

--- Data_structure/2.Queue.py
+++ Data_structure/2.Queue.py
@@ -1,8 +1,35 @@
 class Queue:
     def __init__(self, size=100):
         self.queue = [0 for _ in range(size)]
+        self.size = size
         self.rear = 0
         self.front = 0
 
-    def push(self):
-        
+    def push(self, element):
+        if not self.is_filled():
+            self.rear = (self.rear + 1) % self.size
+            self.queue[self.rear] = element
+        else:
+            raise IndexError("Queue is fully filled.")
+
+    def pop(self):
+        if not self.is_empty():
+            self.front = (self.front + 1) % self.size
+            return self.queue[self.front]
+        else:
+            raise IndexError("Queue is empty.")
+
+    def is_empty(self):
+        return self.rear == self.front
+
+    def is_filled(self):
+        return (self.rear + 1) % self.size == self.front
+
+
+# test
+test_q = Queue(5)
+for i in range(4):
+    test_q.push(i)
+print(test_q.is_filled())
+print(test_q.pop())
+test_q.push(4)
